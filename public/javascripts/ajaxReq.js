e27ef198a713e4611695a41571ed6151cc346718
codecakes <akurnya@gmail.com>
2015-08-12 05:04:53 +0530

    removed commented logic

--- public/javascripts/ajaxReq.js
+++ public/javascripts/ajaxReq.js
@@ -12,28 +12,3 @@ $('#form-reflow').submit(function (e) {
                 });
         return false;
     });
-
-/*
-$('#form-reflow').submit(function (evt) {
-    //how preventDefault and its cousins are different
-    //https://stackoverflow.com/questions/1357118/event-preventdefault-vs-return-false#comment32754627_1357118
-    //then https://jsfiddle.net/APQk6/
-    
-    evt.preventDefault();
-    evt.stopPropagation();
-    console.log("going for $.post");
-    //$("#form-reflow").html('');
-    (function caller() {
-        $.post( $('#form-reflow').attr( 'action' ), 
-                    {data:$("#textinp").val().toLocaleString()
-                    }, 
-                    function onDone (val) {
-                        console.log(val);
-                        $("#form-reflow")
-                        .html(
-                            "<p> hello baby " + val.data + "</p>");
-                    });
-    })();
-    return false;
-    });
-*/
